cdfbb5a6b1eb05daf87c3896a2fd020339ba0f73
Thiago Belini <vsm.thiagobelini@gmail.com>
2021-01-29 21:31:49 -0300

    Commit Inicial BackEnd

---
+++ backend/ambevtech/src/main/java/com/ambevtech/CaseAmbevTechApplication.java
@@ -0,0 +1,13 @@
+package com.ambevtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseAmbevTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseAmbevTechApplication.class, args);
+	}
+
+}
